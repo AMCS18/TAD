58a276a787938fb8e0fc96ed857fdfb0d6ff6fc1
Angel Moises Chi <Angel Moises Chi@LAPTOP-T8VPAKR2>
2023-10-29 02:07:50 -0500

    carga inicial

---
+++ src/tda/Principal.java
@@ -0,0 +1,15 @@
+
+package tda;
+
+public class Principal {
+
+    public static void main(String[] args) {
+     CalculoAngulos obj = new CalculoAngulos();
+     
+     
+     obj.AnguloCentral();
+     obj.AnguloInterior();
+     obj.AnguloExterior();
+    }
+    
+}
